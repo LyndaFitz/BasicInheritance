73dc1c5aca05fbbfd852bdc497f854e37a9461d1
LyndaFitz <lyndaa.fitz@gmail.com>
2024-11-15 16:12:04 -0500

    BASICINHERITANCE

---
+++ Hospital.java
@@ -0,0 +1,17 @@
+
+public class Hospital {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+public class Hospital {
+	   public static void main(String args[]) {
+	       Surgeon s = new Surgeon();
+	       s.Doctor_Details();
+	       s.Surgeon_Details();
+	   }
+	}
+
---
+++ Surgeon.java
@@ -0,0 +1,9 @@
+
+public class Surgeon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
